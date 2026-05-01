c1d6dbaa733079075d86d12f2ca5c520afaebd4e
aytenzordok <aytenzordok89@gmail.com>
2026-05-01 04:40:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 import gymnasium as gym
 import gymnasium_robotics
-import numpy as np
+import torch
 from gym_robotics_custom import RoboGymObservationWrapper
+from model import Actor, Critic
 
-gym.register_envs(gymnasium_robotics)
+env = gym.make('PointMaze_UMaze-v3', render_mode="human")
+env = RoboGymObservationWrapper(env)
 
+actor = Actor(env.observation_space.shape[0], env.action_space.shape[0], 256, env.action_space)
 
-
-if __name__ == '__main__':
-    env_name = "PointMaze_UMaze-v3"
-    max_episode_steps = 100
-
-    STRAIGHT_MAZE = [[1,1,1,1,1],
-                     [1,0,0,0,1],
-                     [1,1,1,1,1]]  # define a maze with empty 3 blocks
-    env = gym.make(env_name, max_episode_steps=max_episode_steps,render_mode="human" ,maze_map=STRAIGHT_MAZE)
-    env=RoboGymObservationWrapper(env)
-    obs,info = env.reset()
-
-    for i in range(1000):
-       action=env.action_space.sample()
-       env.step(action)
-    print(obs,info)
-
+obs, info = env.reset()
+for _ in range(1000):
+    state = torch.FloatTensor(obs).unsqueeze(0)
+    action = actor.sample(state).detach().cpu().numpy()[0]
+    obs, reward, done, truncated, info = env.step(action)
+    if done or truncated:
+        obs, info = env.reset()
+env.close()
